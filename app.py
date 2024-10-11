8ba041b652a1fcf264b6f7a157f8c390907d1e62
Advik <advik.b@gmail.com>
2024-10-11 15:52:30 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from sanic import Sanic
+from sanic.request import Request
+
+app = Sanic(__name__)
+app.config.TEMPLATES_AUTO_RELOAD = True
+app.config.TEMPLATING_PATH_TO_TEMPLATES = "."
+
+
+@app.route("/")
+@app.ext.template("story.html")
+async def index(request: Request):
+    return {}
+
