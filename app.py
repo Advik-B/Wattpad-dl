4335f0f7a28484e1fbdf69b2ffdce54f825598fa
Advik <advik.b@gmail.com>
2024-10-11 18:39:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from sanic import Sanic
 from sanic.request import Request
+from sanic_ext import render
 from sanic.response import json, html, text, file, redirect
 from wattpad import Wattpad
 from wattpad.modals import Story, User
+from wattpad.errors import APIerror
+import json
 
 app = Sanic(__name__)
 app.config.TEMPLATES_AUTO_RELOAD = True
 # app.config.TEMPLATING_PATH_TO_TEMPLATES = "."
 
+wattpad = Wattpad()
 
 @app.route("/")
 @app.ext.template("index.html.j2")
 async def index(request: Request):
-    if request.args.get("story"):
-        story = await Story.from_id(request.args.get("story"))
-        return {"story": story}
-    
-    return text("No story provided in query string")
+    return await render("index.html.j2", context={})
+
+@app.route("/story/<story_id:int>")
+@app.ext.template("story.html.j2")
+async def story(request: Request, story_id: int):
+    try:
+        story = Story.from_id(story_id, wattpad)
+    except APIerror as e:
+        # Render story.error.html.j2 instead
+        return {"error": json.loads(str(e).replace('\'', '"')), "story_id": story_id}
+    return {"story": story}
